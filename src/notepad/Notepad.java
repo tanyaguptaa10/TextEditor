ed01d8634ee894ed7052bc71cc4104b339806bbc
Tanya Gupta <tanyagupta650@gmail.com>
2024-05-30 19:57:51 +0530

    Completed Notepad Project

--- src/notepad/Notepad.java
+++ src/notepad/Notepad.java
@@ -1,8 +1,9 @@
 package notepad;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-import javax.swing. filechooser.*;
+import javax.swing.filechooser.*;
 import java.io.*;
 
 //import rsyntaxtextarea.*;
@@ -10,26 +11,39 @@ import org.fife.ui.rsyntaxtextarea.RSyntaxTextArea;
 import org.fife.ui.rsyntaxtextarea.SyntaxConstants;
 import org.fife.ui.rtextarea.RTextScrollPane;
 
-
 public class Notepad extends JFrame {
+
     private JTabbedPane tabbedPane;
     private int tabCount = 0;
+    private File currentFile; // Track the current file for each tab
+    private Timer  autoSaveTimer;
 
-    Notepad(){
+    Notepad() {
         // Title & image
         setTitle("Tanya_Gupta_Personal_Notepad");
-        ImageIcon notepadIcon = new ImageIcon(ClassLoader.getSystemResource("notepad/icons/notepad.png"));
-        Image icon = notepadIcon.getImage();
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+          setSize(1950, 1050);
+           setBounds(600, 200, 700,600);
+        setLayout(new BorderLayout());
+         Font titleFont = new Font("Arial", Font.BOLD, 28);
+         
+            JPanel titleBarPanel = new JPanel(new BorderLayout());
+        titleBarPanel.setPreferredSize(new Dimension(getWidth(), 60));
+ 
+        // image
+        ImageIcon notepadIcon = new ImageIcon(ClassLoader.getSystemResource("notepad/icons/Notepad_icon.svg.png"));
+        Image icon = notepadIcon.getImage().getScaledInstance(50, 50, Image.SCALE_SMOOTH); // Set the desired icon size
         setIconImage(icon);
-       // Menu Bar - File
-        JMenuBar menubar  = new JMenuBar();
+       
+        // Menu Bar - File
+        JMenuBar menubar = new JMenuBar();
         menubar.setBackground(Color.WHITE);
         JMenu file = new JMenu("File");
         menubar.add(file);
         //FIle Menu items - New tab,new window
         //New Tab
         JMenuItem newTab = new JMenuItem("New Tab");
-        newTab.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N,ActionEvent.CTRL_MASK));//for nemonacs 
+        newTab.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, ActionEvent.CTRL_MASK));//for nemonacs 
         newTab.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 addNewTab();
@@ -40,82 +54,189 @@ public class Notepad extends JFrame {
         add(tabbedPane, BorderLayout.CENTER);
         addNewTab(); // Add initial tab
         // New Window
-         JMenuItem newWindow = new JMenuItem("New Window");
-         newWindow.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_T,ActionEvent.SHIFT_MASK));//
+        JMenuItem newWindow = new JMenuItem("New Window");
+        newWindow.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_T, ActionEvent.SHIFT_MASK));//
         newWindow.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 new Notepad(); // Open a new instance of Notepad when "New Tab" is clicked
             }
         });
-          file.add(newWindow);
+        file.add(newWindow);
         setJMenuBar(menubar);
         //open - menubar
         JMenuItem open = new JMenuItem("Open");
-        open.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O,ActionEvent.CTRL_MASK));//
-         open.addActionListener(new ActionListener() {
+        open.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O, ActionEvent.CTRL_MASK));//
+        open.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 openFile();
             }
         });
-         file.add(open);
-            
-            JMenuItem save = new JMenuItem("Save");
-            save.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S,ActionEvent.CTRL_MASK));//
-              save.addActionListener(new ActionListener() {
-             public void actionPerformed(ActionEvent e) {
-                saveFile();
+        file.add(open);
+
+        JMenuItem save = new JMenuItem("Save");
+        save.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, ActionEvent.CTRL_MASK));//
+        save.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                save();
             }
         });
-           file.add(save);
-         
-         JMenuItem print = new JMenuItem("Print");
-        print.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_P,ActionEvent.CTRL_MASK));//
-         file.add(print);
-         
-         JMenuItem exit = new JMenuItem("Exit");
-        exit.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_E,ActionEvent.CTRL_MASK));//
-         file.add(exit);
+        file.add(save);
+
+        JMenuItem autoSave = new JMenuItem("Auto-Save");
+        autoSave.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_A, ActionEvent.CTRL_MASK));
+        autoSave.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                toggleAutoSave();
+            }
+        });
+        file.add(autoSave);
         
-          // Menu Bar - Edit
+         JMenuItem saveas = new JMenuItem("Save_As");
+        saveas.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_E, ActionEvent.CTRL_MASK));
+        saveas.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                saveAs();
+            }
+        });
+        file.add(saveas);
+       
+
+        JMenuItem exit = new JMenuItem("Exit");
+        exit.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_E, ActionEvent.CTRL_MASK));
+        exit.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+        file.add(exit);
+//        
+        // Menu Bar - Edit
         menubar.setBackground(Color.WHITE);
         JMenu edit = new JMenu("Edit");
         menubar.add(edit);
-        setJMenuBar(menubar);  
-        
+        setJMenuBar(menubar); 
+
         JMenuItem copy = new JMenuItem("Copy");
-        copy.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_C,ActionEvent.CTRL_MASK));//
-        edit.add(copy);  
-        
+        copy.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_C, ActionEvent.CTRL_MASK));
+
+        copy.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                int tabIndex = tabbedPane.getSelectedIndex();
+                Component selectedComponent = tabbedPane.getComponentAt(tabIndex);
+                RTextScrollPane scrollPane = (RTextScrollPane) selectedComponent;
+                RSyntaxTextArea textArea = (RSyntaxTextArea) scrollPane.getViewport().getView();
+                textArea.copy();
+            }
+        });
+        edit.add(copy);
+
         JMenuItem paste = new JMenuItem("Paste");
-        paste.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_V,ActionEvent.CTRL_MASK));//
-        edit.add(paste); 
-        
+        paste.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_V, ActionEvent.CTRL_MASK));
+        paste.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                int tabIndex = tabbedPane.getSelectedIndex();
+                Component selectedComponent = tabbedPane.getComponentAt(tabIndex);
+                RTextScrollPane scrollPane = (RTextScrollPane) selectedComponent;
+                RSyntaxTextArea textArea = (RSyntaxTextArea) scrollPane.getViewport().getView();
+                textArea.paste();
+            }
+        });
+        edit.add(paste);
+
         JMenuItem cut = new JMenuItem("Cut");
-        cut.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_K,ActionEvent.CTRL_MASK));//
-        edit.add(cut); 
-        
-         JMenuItem selectAll = new JMenuItem("Select All");
-        selectAll.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_A,ActionEvent.CTRL_MASK));//
-        edit.add(selectAll); 
-        
+        cut.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_K, ActionEvent.CTRL_MASK));
+        cut.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                int tabIndex = tabbedPane.getSelectedIndex();
+                Component selectedComponent = tabbedPane.getComponentAt(tabIndex);
+                RTextScrollPane scrollPane = (RTextScrollPane) selectedComponent;
+                RSyntaxTextArea textArea = (RSyntaxTextArea) scrollPane.getViewport().getView();
+                textArea.cut();
+            }
+        });
+        edit.add(cut);
+
+        JMenuItem selectAll = new JMenuItem("Select All");
+        selectAll.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_A, ActionEvent.CTRL_MASK));
+        selectAll.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                int tabIndex = tabbedPane.getSelectedIndex();
+                Component selectedComponent = tabbedPane.getComponentAt(tabIndex);
+                RTextScrollPane scrollPane = (RTextScrollPane) selectedComponent;
+                RSyntaxTextArea textArea = (RSyntaxTextArea) scrollPane.getViewport().getView();
+                textArea.selectAll();
+            }
+        });
+        edit.add(selectAll);
+
         //Menu Bar - View
         menubar.setBackground(Color.WHITE);
         JMenu view = new JMenu("View");
         menubar.add(view);
         setJMenuBar(menubar);
 
-        
-        setExtendedState(JFrame.MAXIMIZED_BOTH); 
+
+        JMenuItem zoomIn = new JMenuItem("Zoom In");
+        zoomIn.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_EQUALS, ActionEvent.CTRL_MASK));
+        zoomIn.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                // Get the currently selected tab's RSyntaxTextArea
+                int tabIndex = tabbedPane.getSelectedIndex();
+                Component selectedComponent = tabbedPane.getComponentAt(tabIndex);
+                RTextScrollPane scrollPane = (RTextScrollPane) selectedComponent;
+                RSyntaxTextArea textArea = (RSyntaxTextArea) scrollPane.getViewport().getView();
+
+                // Increase the font size
+                Font currentFont = textArea.getFont();
+                int currentSize = currentFont.getSize();
+                int newSize = currentSize + 2; // Increase the font size by 2 points
+                textArea.setFont(new Font(currentFont.getFontName(), currentFont.getStyle(), newSize));
+            }
+        });
+        view.add(zoomIn);
+
+        JMenuItem zoomOut = new JMenuItem("Zoom Out");
+        zoomOut.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_MINUS, ActionEvent.CTRL_MASK));
+        zoomOut.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                // Get the currently selected tab's RSyntaxTextArea
+                int tabIndex = tabbedPane.getSelectedIndex();
+                Component selectedComponent = tabbedPane.getComponentAt(tabIndex);
+                RTextScrollPane scrollPane = (RTextScrollPane) selectedComponent;
+                RSyntaxTextArea textArea = (RSyntaxTextArea) scrollPane.getViewport().getView();
+
+                // Decrease the font size
+                Font currentFont = textArea.getFont();
+                int currentSize = currentFont.getSize();
+                int newSize = currentSize - 2; // Decrease the font size by 2 points
+                textArea.setFont(new Font(currentFont.getFontName(), currentFont.getStyle(), newSize));
+            }
+        });
+        view.add(zoomOut);
+
+
+
+        setExtendedState(JFrame.MAXIMIZED_BOTH);
         setVisible(true);
     }
+
     // addNEwtab function
     private void addNewTab() {
-        JTextArea textArea = new JTextArea();
-        JScrollPane scrollPane = new JScrollPane(textArea);
+//        JTextArea textArea = new JTextArea();
+//        JScrollPane scrollPane = new JScrollPane(textArea);
+//        tabCount++;
+//        tabbedPane.addTab("Untitled " + tabCount, scrollPane);
+        RSyntaxTextArea textArea1 = new RSyntaxTextArea();
+        textArea1.setSyntaxEditingStyle(SyntaxConstants.SYNTAX_STYLE_JAVA); // Set syntax highlighting for Java
+        textArea1.setCodeFoldingEnabled(true); // Enable code folding
+        RTextScrollPane scrollPane = new RTextScrollPane(textArea1);
         tabCount++;
         tabbedPane.addTab("Untitled " + tabCount, scrollPane);
     }
-     private void openFile() {
+
+    private void openFile() {
+
+        // private void openFile() {
         JFileChooser fileChooser = new JFileChooser();
         int result = fileChooser.showOpenDialog(this);
         if (result == JFileChooser.APPROVE_OPTION) {
@@ -126,38 +247,74 @@ public class Notepad extends JFrame {
                 while ((line = reader.readLine()) != null) {
                     content.append(line).append("\n");
                 }
-                JTextArea textArea = new JTextArea(content.toString());
-                JScrollPane scrollPane = new JScrollPane(textArea);
+                RSyntaxTextArea textArea = new RSyntaxTextArea(content.toString());
+                textArea.setSyntaxEditingStyle(SyntaxConstants.SYNTAX_STYLE_JAVA); // Set syntax highlighting for Java
+                textArea.setCodeFoldingEnabled(true); // Enable code folding
+                RTextScrollPane scrollPane = new RTextScrollPane(textArea);
                 tabCount++;
                 tabbedPane.addTab(file.getName(), scrollPane);
-                  }catch(IOException ex){
-                    ex.printStackTrace();
-                }
+            } catch (IOException ex) {
+                ex.printStackTrace();
             }
         }
- private void saveFile() {
+    }
+
+    private void save() {
+        if (currentFile != null) {
+            int tabIndex = tabbedPane.getSelectedIndex();
+            Component selectedComponent = tabbedPane.getComponentAt(tabIndex);
+            RTextScrollPane scrollPane = (RTextScrollPane) selectedComponent;
+            RSyntaxTextArea textArea = (RSyntaxTextArea) scrollPane.getViewport().getView();
+            String content = textArea.getText();
+
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(currentFile))) {
+                writer.write(content);
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        } else {
+            saveAs();
+        }
+    }
+
+    private void saveAs() {
         int tabIndex = tabbedPane.getSelectedIndex();
         Component selectedComponent = tabbedPane.getComponentAt(tabIndex);
-        JScrollPane scrollPane = (JScrollPane) selectedComponent;
-        JTextArea textArea = (JTextArea) scrollPane.getViewport().getView();
+        RTextScrollPane scrollPane = (RTextScrollPane) selectedComponent;
+        RSyntaxTextArea textArea = (RSyntaxTextArea) scrollPane.getViewport().getView();
         String content = textArea.getText();
 
         JFileChooser fileChooser = new JFileChooser();
         int result = fileChooser.showSaveDialog(this);
         if (result == JFileChooser.APPROVE_OPTION) {
-            File file = fileChooser.getSelectedFile();
-            try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
+            currentFile = fileChooser.getSelectedFile();
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(currentFile))) {
                 writer.write(content);
             } catch (IOException ex) {
                 ex.printStackTrace();
             }
         }
     }
-              
     
+    
+    private void toggleAutoSave() {
+        if (autoSaveTimer == null) {
+            autoSaveTimer = new Timer(3000, new ActionListener() { // Auto-save every 30 seconds
+                public void actionPerformed(ActionEvent e) {
+                    save();
+                }
+            });
+            autoSaveTimer.start();
+        } else {
+            autoSaveTimer.stop();
+            autoSaveTimer = null;
+        }
+    }
 
     public static void main(String[] args) {
         new Notepad();
     }
 }
- 
+
+
+
